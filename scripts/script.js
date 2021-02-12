e27427807cb6219a01f96f1e3ce682de5eeb3f5e
s1duan <43221167+s1duan@users.noreply.github.com>
2021-02-11 19:03:08 -0800

    js fetch

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,13 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // check if local storage already contains the data
-  if (window.localStorage.length == 0){
-    fetch('https://fakestoreapi.com/products')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        for (var key in data) {
-          console.log(data[key])
-          window.localStorage.setItem(key, JSON.stringify(data[key]))
-        }
-      })
-  }
+  fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      for (var key in data) {
+        console.log(data[key])
+        window.localStorage.setItem(key, JSON.stringify(data[key]))
+      }
+    })
 });
